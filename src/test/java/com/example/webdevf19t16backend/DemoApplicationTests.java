4b920ddb7200348bc5d2cee413f57a8007aab40f
Stephen C <costa.ste@husky.neu.edu>
2019-11-30 13:57:53 -0500

    spring init

---
+++ src/test/java/com/example/webdevf19t16backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdevf19t16backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
